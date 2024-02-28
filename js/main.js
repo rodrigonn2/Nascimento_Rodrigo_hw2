4238453ed8d9b99d53976c227e31ce3b99e75a07
rodrigonn2 <144962992+rodrigonn2@users.noreply.github.com>
2024-02-28 17:25:17 -0500

    updating jS to reset the puzzle

--- js/main.js
+++ js/main.js
@@ -18,15 +18,32 @@ let theButtons = document.querySelectorAll("#buttonHolder img"),
 // step 3
 // functionality always goes in the middle -> how do we want
 // the app to behave?
+
 function changeBGImage() {
 	// the `` is a JavaScript template string. It tells the JS enging to evaluate the expression
 	// inside the braces - run that little bit of code. In this case it's just pulling the ID of the
 	// button we clicked on and putting it at the end of the image name (0, 1, 2, 3)
 	// and updating the background-image style of the puzzle board element.
-
+	
 	// bug fix #2 should go here. it's at most 3 lines of JS code.
+
+	dropZones.forEach(zone => {
+        if (zone.children.length > 0) {
+            zone.removeChild(zone.children[0]);
+            console.log('reseting the puzzle');
+			puzzlePieces = null;
+  
+        }
+
+    });
+
 	puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
+	
+
+
+
 }
+
 
 function handleStartDrag() { 
 	console.log('started dragging this piece:', this);
